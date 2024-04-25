af4d635c563ef977bd0bd56b0cf3ddb51aa2dd29
pol <33850434+polcamden@users.noreply.github.com>
2024-04-24 21:39:17 -0500

    codon and genes to file

--- Genome Analysis/src/GenomeAnalyzer.java
+++ Genome Analysis/src/GenomeAnalyzer.java
@@ -143,6 +143,13 @@ public class GenomeAnalyzer {
         return genes.size();
     }
 
+    public String getSequenceFileName(){
+
+        String str = sequenceFile.getName();
+
+        return str.substring(0, str.length() - 4);
+    }
+
     @Override
     public String toString() {
         return "GenomeAnalyzer File " + sequenceFile;
